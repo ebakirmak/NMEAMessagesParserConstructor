706682f249fec829de23770c1861ac68d571ad04
Emre Berk AKIRMAK <ebakirmak@gmail.com>
2017-07-13 16:01:15 +0300

    We starting NMEA Project

---
+++ NMEA_MessageParserConstructor/MessageType1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMEA_MessageParserConstructor
+{
+    public class MessageType1:RootMessages
+    {
+        public MessageType1()
+        {
+            this.MessageID = 1;
+            this.Description = "Scheduled position report";
+            this.RepeatIndicator = 0;
+            this.Priority = 1;
+            this.TotalNumberOfBits = 168;
+            
+        }
+        //MMSI
+        private int UserID { get; set; }
+        private byte NavigationalStatus { get; set; }
+        private byte RateOfTurnROTAIS { get; set; }
+        private float SOG { get; set; }
+        private byte PositionAccuracy { get; set; }
+        private float Longitude { get; set; }
+        private float Latitude { get; set; }
+        private float COG { get; set; }
+        private int TrueHeading { get; set; }
+        private byte TimeStamp { get; set; }
+        private int SpecialManoeuvreIndicator { get; set; }
+        private int RAIMFlag { get; set; }
+        
+
+    }
+}
---
+++ NMEA_MessageParserConstructor/RootMessages.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMEA_MessageParserConstructor
+{
+    public abstract class RootMessages
+    {
+        public RootMessages()
+        {
+            this.RepeatIndicator = 0;
+        }
+        protected string MessageID { get; set; }
+
+        protected string Description { get; set; }
+
+        public string Priority { get; set; }
+
+        protected string AccessSchema { get; set; }
+
+        protected string CommunicationState { get; set; }
+
+        protected string TransmittedStation { get; set; }
+
+        protected byte RepeatIndicator { get; set; }
+
+        protected string NumberOfBits { get; set; }
+
+        protected int Spare { get; set; }
+
+        enum enumPriority
+        {
+          PriorityOne,
+          PriorityTwo,
+          PriorityThree,    
+          PriorityFour
+
+        };
+
+        enum enumAccessSchema
+        {
+            SOTDMA,
+            RATDMA,
+            CSTDMA,
+            MSSA,
+            FATDMA,
+            ITDMA
+        };
+
+        enum enumCommunicitonState {
+            SOTDMA,
+            ITDMA,
+            NA
+        };
+
+        enum enumTransmittedStation
+        {
+            M,
+            B
+        };
+    }
+}
